19863ca6c8f2ba2d6bcfea7ba225c44249510696
InjamulRashik <ihrashik@gmail.com>
2021-07-17 23:35:38 +0600

    Slicer Updated!

---
+++ ArraySlice.js
@@ -0,0 +1,7 @@
+const first = [1, 2, 3, 4];
+const second = [5, 6, 7, 8];
+
+//const combined = first.concat(second); // Joined
+// const join = first.join();
+// const slice = combined.slice(2, 4);
+// console.log(join);
