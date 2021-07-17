903d755e3929c4b0a8a9d357d0116eedfd2f5f0d
InjamulRashik <ihrashik@gmail.com>
2021-07-18 01:57:43 +0600

    Updated!

---
+++ Reduce.js
@@ -0,0 +1,6 @@
+const numbers = [2, 3, -6, 8];
+
+const output = numbers.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+}, 0); // Here 0 is the value of accumulator just like sum=0 and sum=sum+1
+console.log(output);
